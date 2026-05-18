d04fe5b99afc234bf05e1ff245c1ae50fb69734f
iReink <ireink@gmail.com>
2026-05-19 00:05:37 +0500

    Configure Decap OAuth endpoints

--- deploy.sh
+++ deploy.sh
@@ -46,6 +46,22 @@ server {
         proxy_set_header X-Real-IP \$remote_addr;
     }
 
+    location = /auth {
+        proxy_pass http://127.0.0.1:3111/auth;
+        proxy_set_header Host \$host;
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto \$scheme;
+    }
+
+    location = /callback {
+        proxy_pass http://127.0.0.1:3111/callback;
+        proxy_set_header Host \$host;
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto \$scheme;
+    }
+
     location / {
         return 301 https://\$host\$request_uri;
     }
@@ -70,6 +86,22 @@ server {
         proxy_set_header X-Real-IP \$remote_addr;
     }
 
+    location = /auth {
+        proxy_pass http://127.0.0.1:3111/auth;
+        proxy_set_header Host \$host;
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto \$scheme;
+    }
+
+    location = /callback {
+        proxy_pass http://127.0.0.1:3111/callback;
+        proxy_set_header Host \$host;
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto \$scheme;
+    }
+
     location / {
         try_files \$uri \$uri/ /index.html;
     }
@@ -89,6 +121,22 @@ server {
         proxy_pass http://127.0.0.1:9000/hooks/deploy-portfolio;
         proxy_set_header Host \$host;
         proxy_set_header X-Real-IP \$remote_addr;
+    }
+
+    location = /auth {
+        proxy_pass http://127.0.0.1:3111/auth;
+        proxy_set_header Host \$host;
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto \$scheme;
+    }
+
+    location = /callback {
+        proxy_pass http://127.0.0.1:3111/callback;
+        proxy_set_header Host \$host;
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto \$scheme;
     }
 
     location / {
